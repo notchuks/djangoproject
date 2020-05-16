69472ddb24cccaa50fce018414d94990e7a71173
Chukwuka <chukaezem@gmail.com>
2020-05-16 20:42:44 +0100

    check

--- chukaezemscrumy/views.py
+++ chukaezemscrumy/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse("Welcome to Django")
+    return HttpResponse("This is a Scrum Application")
 
 # Create your views here.
