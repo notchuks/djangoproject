0f658ca6049d1f4ecf0af0617dd5e07facad9676
Chukwuka <chukaezem@gmail.com>
2020-05-18 15:26:02 +0100

    lab 8 redo

--- chukaezemscrumy/models.py
+++ chukaezemscrumy/models.py
@@ -1,16 +1,17 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
 
 class GoalStatus(models.Model):
-    status_name = models.CharField(max_length=50)
+    status_name = models.CharField(max_length=10)
 
 class ScrumyGoals(models.Model):
     goal_name = models.CharField(max_length=50)
-    goal_id = models.IntegerField()
+    goal_id = models.IntegerField(default=1)
     created_by = models.CharField(max_length=50)
     moved_by = models.CharField(max_length=50)
     owner = models.CharField(max_length=50)
-    goal_status = models.ForeignKey(GoalStatus, on_delete = models.PROTECT, related_name='goalstatus')
+    goal_status = models.ForeignKey(GoalStatus, on_delete = models.PROTECT)
     user = models.ForeignKey(User, related_name='scrumygoals', on_delete = models.PROTECT)
 
 class ScrumyHistory(models.Model):
@@ -18,8 +19,8 @@ class ScrumyHistory(models.Model):
     created_by = models.CharField(max_length=50)
     moved_from = models.CharField(max_length=50)
     moved_to = models.CharField(max_length=50)
-    time_of_action = models.IntegerField()
-    goal = models.ForeignKey(ScrumyGoals, on_delete = models.PROTECT, related_name='scrumygoals')
+    time_of_action = models.DateField()
+    goal = models.ForeignKey(ScrumyGoals, on_delete = models.CASCADE, related_name='scrumygoals')
 
 
 # Create your models here.
