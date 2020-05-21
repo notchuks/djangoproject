21c80a46c5821be0733ed6374946a8be0b132087
Chukwuka <chukaezem@gmail.com>
2020-05-21 07:27:35 +0100

    lab 8 & 9

--- chukaezemscrumy/models.py
+++ chukaezemscrumy/models.py
@@ -5,6 +5,10 @@ from django.contrib.auth.models import User
 class GoalStatus(models.Model):
     status_name = models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.status_name
+
+
 class ScrumyGoals(models.Model):
     goal_name = models.CharField(max_length=50)
     goal_id = models.IntegerField(default=1)
@@ -12,7 +16,10 @@ class ScrumyGoals(models.Model):
     moved_by = models.CharField(max_length=50)
     owner = models.CharField(max_length=50)
     goal_status = models.ForeignKey(GoalStatus, on_delete = models.PROTECT)
-    user = models.ForeignKey(User, related_name='scrumygoals', on_delete = models.PROTECT)
+    user = models.ForeignKey(User, related_name='goal_created', on_delete = models.PROTECT)
+
+    def __str__(self):
+        return self.goal_name
 
 class ScrumyHistory(models.Model):
     moved_by = models.CharField(max_length=50)
@@ -20,7 +27,10 @@ class ScrumyHistory(models.Model):
     moved_from = models.CharField(max_length=50)
     moved_to = models.CharField(max_length=50)
     time_of_action = models.DateField()
-    goal = models.ForeignKey(ScrumyGoals, on_delete = models.CASCADE, related_name='scrumygoals')
+    goal = models.ForeignKey(ScrumyGoals, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.created_by
 
 
 # Create your models here.
